sjg20/u-boot-concept

cmd: run: add "-e" option to run an EFI application
"run -e" allows for executing EFI application with a specific "BootXXXX"
variable. If no "BootXXXX" is specified or "BootOrder" is specified,
it tries to run an EFI application specified in the order of "bootOrder."

Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>

cmd/nvedit.c
@@ -1344,7 +1344,14 @@ U_BOOT_CMD_COMPLETE(
 	run,	CONFIG_SYS_MAXARGS,	1,	do_run,
 	"run commands in an environment variable",
 	"var [...]\n"
-	"    - run the commands in the environment variable(s) 'var'",
+	"    - run the commands in the environment variable(s) 'var'"
+#if defined(CONFIG_CMD_BOOTEFI)
+	"\n"
+	"run -e [BootXXXX]\n"
+	"    - load and run UEFI app based on 'BootXXXX' UEFI variable",
+#else
+	,
+#endif
 	var_complete
 );
 #endif

common/cli.c
@@ -12,6 +12,7 @@
 #include <cli.h>
 #include <cli_hush.h>
 #include <console.h>
+#include <efi_loader.h>
 #include <fdtdec.h>
 #include <malloc.h>
 
@@ -124,6 +125,15 @@ int do_run(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
+
+#ifdef CONFIG_CMD_BOOTEFI
+	if (!strcmp(argv[1], "-e")) {
+		argc--;
+		argv++;
+
+		return do_bootefi_run(cmdtp, flag, argc, argv);
+	}
+#endif
 
 	for (i = 1; i < argc; ++i) {
 		char *arg;

t-akashi/u-boot-for-travis

cmd/nvedit.c
@@ -1370,7 +1370,14 @@ U_BOOT_CMD_COMPLETE(
 	run,	CONFIG_SYS_MAXARGS,	1,	do_run,
 	"run commands in an environment variable",
 	"var [...]\n"
-	"    - run the commands in the environment variable(s) 'var'",
+	"    - run the commands in the environment variable(s) 'var'"
+#if defined(CONFIG_CMD_BOOTEFI_RUN)
+	"\n"
+	"run -e [BootXXXX]\n"
+	"    - load and run UEFI app based on 'BootXXXX' UEFI variable",
+#else
+	,
+#endif
 	var_complete
 );
 #endif

common/cli.c
@@ -12,6 +12,7 @@
 #include <cli.h>
 #include <cli_hush.h>
 #include <console.h>
+#include <efi_loader.h>
 #include <fdtdec.h>
 #include <malloc.h>
 
@@ -124,6 +125,15 @@ int do_run(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
+
+#ifdef CONFIG_CMD_BOOTEFI_RUN
+	if (!strcmp(argv[1], "-e")) {
+		argc--;
+		argv++;
+
+		return do_run_efi(cmdtp, flag, argc, argv);
+	}
+#endif
 
 	for (i = 1; i < argc; ++i) {
 		char *arg;