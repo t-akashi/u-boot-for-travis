3e4402565731c5e2f4dd29e40214500b8c3db762
AKASHI Takahiro <takahiro.akashi@linaro.org>
2019-07-31 13:47:01 +0900

    disk: converted with new env interfaces
    env_xxx(...) -> env_xxx(ctx_uboot, ...)
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>

--- disk/part.c
+++ disk/part.c
@@ -491,7 +491,7 @@ int blk_get_device_part_str(const char *ifname, const char *dev_part_str,
 	/* If no dev_part_str, use bootdevice environment variable */
 	if (!dev_part_str || !strlen(dev_part_str) ||
 	    !strcmp(dev_part_str, "-"))
-		dev_part_str = env_get("bootdevice");
+		dev_part_str = env_get(ctx_uboot, "bootdevice");
 
 	/* If still no dev_part_str, it's an error */
 	if (!dev_part_str) {
